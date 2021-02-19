ec3592dd6254c56a402a7a61279bbb78420b2867
XShellv <18217706879@163.com>
2021-02-19 21:38:10 +0800

    update

--- 7.整数反转.js
+++ 7.整数反转.js
@@ -9,8 +9,16 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-
+var reverse = function (x) {
+    const xStr = x.toString();
+    const xReverseStr = xStr.split("").reverse().join("");
+    if (xReverseStr[xReverseStr.length - 1] === "-") {
+        const ret = -1 * xReverseStr.slice(0, xReverseStr.length - 1);
+        return ret < Math.pow(-2, 31) ? 0 : ret
+    } else {
+        const ret = 1 * xReverseStr.slice(0, xReverseStr.length);
+        return ret > Math.pow(2, 31) - 1 ? 0 : ret
+    }
 };
 // @lc code=end
 
